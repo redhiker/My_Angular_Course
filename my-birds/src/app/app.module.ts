b8263bea258fe2f778e02eddd4f8f47e4df5b07a
Larry <some@email.com>
2019-12-03 15:49:11 -0500

    commit

--- my-birds/src/app/app.module.ts
+++ my-birds/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BirdListComponent } from './birds/bird-list/bird-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BirdListComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-birds/src/app/birds/bird-list/bird-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bird-list',
+  templateUrl: './bird-list.component.html',
+  styleUrls: ['./bird-list.component.css']
+})
+export class BirdListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
