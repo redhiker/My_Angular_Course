d0086d703e654df1a2b06fb1cfcb8efe565bc309
Larry <some@email.com>
2019-12-04 13:29:43 -0500

    commit

--- my-birds/src/app/birds/bird-list/bird-list.component.ts
+++ my-birds/src/app/birds/bird-list/bird-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Bird } from '../bird.model';
 
+import { BIRDS } from "../birds.data";
+
 @Component({
   selector: 'app-bird-list',
   templateUrl: './bird-list.component.html',
@@ -15,29 +17,7 @@ export class BirdListComponent implements OnInit {
 
   ngOnInit() {
 
-    this.birdsArray = [
-      {
-        id: 1,
-        name: "Cardinal",
-        kind: "Songbird",
-        niche: "Perching Bird nests in trees.",
-        url: "https://www.audubon.org/sites/default/files/styles/grid_gallery_lightbox/public/Red-tailed_Hawk_r25-8-001_l.jpg?itok=6fzB2emF"
-      },
-      {
-        id: 2,
-        name: "Red-tailed Hawk",
-        kind: "Raptor",
-        niche: "Has very good eyesight to spot small game.",
-        url: "https://www.audubon.org/sites/default/files/styles/grid_gallery_lightbox/public/Red-tailed_Hawk_r25-8-001_l.jpg?itok=6fzB2emF"
-      },
-      {
-        id: 3,
-        name: "Pileated Woodpecker",
-        kind: "Woodpecker",
-        niche: "Likes trees.",
-        url: "https://www.audubon.org/sites/default/files/styles/grid_gallery_lightbox/public/Red-tailed_Hawk_r25-8-001_l.jpg?itok=6fzB2emF"
-      }
-    ];
+    this.birdsArray = BIRDS;
   }
 
   parentFunctionHandler(bird) {
---
+++ my-birds/src/app/birds/birds.data.ts
@@ -0,0 +1,25 @@
+import { Bird } from "./bird.model";
+
+export const BIRDS: Bird[] = [
+  {
+    id: 1,
+    name: "Cardinal",
+    kind: "Songbird",
+    niche: "Perching Bird nests in trees.",
+    url: "https://www.audubon.org/sites/default/files/styles/grid_gallery_lightbox/public/Red-tailed_Hawk_r25-8-001_l.jpg?itok=6fzB2emF"
+  },
+  {
+    id: 2,
+    name: "Red-tailed Hawk",
+    kind: "Raptor",
+    niche: "Has very good eyesight to spot small game.",
+    url: "https://www.audubon.org/sites/default/files/styles/grid_gallery_lightbox/public/Red-tailed_Hawk_r25-8-001_l.jpg?itok=6fzB2emF"
+  },
+  {
+    id: 3,
+    name: "Pileated Woodpecker",
+    kind: "Woodpecker",
+    niche: "Likes trees.",
+    url: "https://www.audubon.org/sites/default/files/styles/grid_gallery_lightbox/public/Red-tailed_Hawk_r25-8-001_l.jpg?itok=6fzB2emF"
+  }
+];
